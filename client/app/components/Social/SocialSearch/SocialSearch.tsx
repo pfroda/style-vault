89c31d976ab60ac2bafa806f750d1e27c5637f1a
Pau Fàbrega <paufabregaroda@gmail.com>
2023-10-18 11:01:18 +0200

    social search bar

--- client/app/components/Social/SocialSearch/SocialSearch.tsx
+++ client/app/components/Social/SocialSearch/SocialSearch.tsx
@@ -1,45 +1,77 @@
 import './socialsearch.css';
-import searchUser from '../../../../public/search-user.png';
+import searchIcon from '../../../../public/search-user.png';
 import userImage from '../../../../public/user.png';
 import Image from 'next/image';
 import { useState } from 'react';
 import { queryUsersForSearch } from '@/app/services/apiGraphQL';
+import useAuth from '@/app/hooks/useAuth';
 
 function SocialSearch() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<User[]>([]);
+  const { user } = useAuth();
+  let searchTimeout: any;
+  
+  const handleInputChange = (e) => {
+    const query = e.target.value;
+    //delay the api call until user typed more
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      handleSearch(query);
+    }, 300);
+  };
 
-  const handleSearchChange = async (event) => {
-    const newSearch = event.target.value;
-    setSearchTerm(newSearch);
+  const handleSearch = async (query: string) => {
+    if (query.trim() === '') {
+      setSearchResults([]);
+      return;
+    }
 
-    const result: any = await queryUsersForSearch(newSearch);
-    console.log(result);
-    console.log('filtered results', result.data.getAllUsers)
-    setSearchResults(result.data.getAllUsers)
+    try {
+      const searchedUsers = await queryUsersForSearch(query);
+      const matchedUsers = searchedUsers?.data?.getAllUsers.filter((user) =>
+        user.username.toLowerCase().includes(query.toLowerCase()));
+      
+      const filteredResults = matchedUsers?.filter((match) => match.id !== user?.id)
+      console.log('userid:', user?.id)
+      console.log('matched users:', matchedUsers);
+      console.log('filtered results;', filteredResults);
+
+      if (filteredResults) setSearchResults(filteredResults);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleResultClick = () => {
+    console.log('clicked mf');
   }
 
   return (
     <div className='SocialSearch'>
       <div className="social-header-container">
-        <Image src={searchUser} alt="" className='search-user' />
+        <Image src={searchIcon} alt="search" className='search-user' />
         <input
-        type="text"
-        placeholder='Search user...'
-        value={searchTerm}
-        onChange={handleSearchChange}/>
+          type="text"
+          placeholder='Search user...'
+          onChange={handleInputChange}
+        />
       </div>
       <div className="social-results">
-      {searchResults.map((user) => (
-          <div className="user-result" key={user?.id}>
-            <Image alt="profile"
-            src={user?.profilePicture}
-            className='profile-image'
-            width={100}
-            height={20}/>
-            <div className="profile-username">@{user?.username}</div>
-          </div>
-        ))}
+        {searchResults.length > 0 && (
+          searchResults.map((user) => (
+            <div className="user-result" key={user.id} onClick={handleResultClick}>
+              <Image
+                alt="profile"
+                src={user.profilePicture || userImage}
+                className='profile-image'
+                width={100}
+                height={100}
+              />
+              <div className="profile-username">@{user.username || 'Unknown'}</div>
+            </div>
+          )).slice(0,4)
+        )}
       </div>
     </div>
   );
