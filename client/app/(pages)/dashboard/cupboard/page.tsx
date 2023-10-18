80fc3eca41611d42f27a6bf1fafbdc6bf765fc1d
Xavier Fabrega Pascual <xavifabrega2@hotmail.com>
2023-10-18 11:55:02 +0200

    feat: SocialProfile Component

--- client/app/(pages)/dashboard/cupboard/page.tsx
+++ client/app/(pages)/dashboard/cupboard/page.tsx
@@ -1,11 +1,15 @@
 'use client'
-import Closet from "@/app/components/Closet/Closet"
-import Footer from "@/app/components/Footer/Footer"
+import Closet from "@/app/components/Closet/Closet";
+import Footer from "@/app/components/Footer/Footer";
+import useAuth from "@/app/hooks/useAuth";
 
 function index() {
+  const { user } = useAuth();
+
   return (
     <>
-      <Closet />
+      {/* <Closet isOwnProfile={false} /> */}
+      <Closet isOwnProfile={true} userId={user?.id!} />
       <Footer />
     </>
   )
